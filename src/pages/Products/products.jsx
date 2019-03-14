33df11cd30aa5d7055db8224e1a3bf1e20c12bab
baadillahnabil <nabilzero27@gmail.com>
2019-03-14 17:22:34 +0700

    add products page

---
+++ src/pages/Products/products.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import classes from './products.module.scss'
+
+class Products extends Component {
+  render() {
+    return (
+      <>
+        <p>This is products page</p>
+      </>
+    )
+  }
+}
+
+export default Products
