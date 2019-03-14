1704959054bb024addbdc71715abf69073466b27
baadillahnabil <nabilzero27@gmail.com>
2019-03-14 21:57:37 +0700

    add dummy cards

--- src/pages/Products/products.jsx
+++ src/pages/Products/products.jsx
@@ -1,12 +1,40 @@
 import React, { Component } from 'react'
+import { Container, Grid, Card, Image, Icon } from 'semantic-ui-react'
 import classes from './products.module.scss'
 
 class Products extends Component {
   render() {
     return (
-      <>
-        <p>This is products page</p>
-      </>
+      <div className={classes.productsPage}>
+        <Container fluid>
+          <Grid>
+            <Grid.Row>
+              {Array.from(Array(10), (el, index) => (
+                <Grid.Column width={4} key={index} className={classes.card}>
+                  <Card centered>
+                    <Image src="https://react.semantic-ui.com/images/avatar/large/matthew.png" />
+                    <Card.Content>
+                      <Card.Header>Matthew</Card.Header>
+                      <Card.Meta>
+                        <span className="date">Joined in 2015</span>
+                      </Card.Meta>
+                      <Card.Description>
+                        Matthew is a musician living in Nashville.
+                      </Card.Description>
+                    </Card.Content>
+                    <Card.Content extra>
+                      <p>
+                        <Icon name="user" />
+                        22 Friends
+                      </p>
+                    </Card.Content>
+                  </Card>
+                </Grid.Column>
+              ))}
+            </Grid.Row>
+          </Grid>
+        </Container>
+      </div>
     )
   }
 }
