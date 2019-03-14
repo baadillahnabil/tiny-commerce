2773c382b9a1cca9b5ea1fde6f53103938ba92b9
baadillahnabil <nabilzero27@gmail.com>
2019-03-14 17:17:52 +0700

    change path

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.scss'
-import App from './Root/app'
+import App from './root/app'
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
