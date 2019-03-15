877ff37ce71d1978de9565e540da537add4fd6cc
baadillahnabil <nabilzero27@gmail.com>
2019-03-15 16:22:11 +0700

    show cart items in a table

--- src/pages/Carts/carts.jsx
+++ src/pages/Carts/carts.jsx
@@ -1,20 +1,78 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Container } from 'semantic-ui-react'
+import { Container, Message, Table, Header, Button } from 'semantic-ui-react'
 
 import classes from './carts.module.scss'
 import { addToCart, resetCart } from '../../store/actions/actions'
 
 class Carts extends Component {
-  componentDidMount() {
-    console.log(this.props.carts)
-  }
-
   render() {
+    const { carts, resetCart } = this.props
+
+    let totalPrice = 0
+    if (carts.length !== 0) {
+      for (const product of carts) {
+        totalPrice += Number(product.price)
+      }
+      totalPrice = `${carts[0].currency} ${totalPrice.toFixed(2)}`
+    }
+
     return (
-      <div className={classes.productsPage}>
-        <Container fluid>
-          <p>Hello</p>
+      <div className={classes.cartsPage}>
+        <Container>
+          {carts.length === 0 ? (
+            <Message>
+              <Message.Header className={classes.emptyMessage}>
+                Cart is empty :)
+              </Message.Header>
+            </Message>
+          ) : (
+            <>
+              <Table celled>
+                <Table.Header>
+                  <Table.Row>
+                    <Table.HeaderCell className={classes.columnIndex}>
+                      #
+                    </Table.HeaderCell>
+                    <Table.HeaderCell>Product Name</Table.HeaderCell>
+                    <Table.HeaderCell className={classes.columnPriceHeader}>
+                      Price
+                    </Table.HeaderCell>
+                  </Table.Row>
+                </Table.Header>
+
+                <Table.Body>
+                  {carts.map((product, index) => (
+                    <Table.Row key={index}>
+                      <Table.Cell>{index + 1}</Table.Cell>
+                      <Table.Cell>{product.name}</Table.Cell>
+                      <Table.Cell className={classes.columnPrice}>
+                        {product.currency} {product.price}
+                      </Table.Cell>
+                    </Table.Row>
+                  ))}
+                </Table.Body>
+
+                <Table.Footer fullWidth>
+                  <Table.Row>
+                    <Table.HeaderCell colSpan="2">
+                      <Header floated="right">Total : </Header>
+                    </Table.HeaderCell>
+                    <Table.HeaderCell>
+                      <Header floated="right">{totalPrice}</Header>
+                    </Table.HeaderCell>
+                  </Table.Row>
+                </Table.Footer>
+              </Table>
+              <br />
+              <Button floated="right" color="red" inverted>
+                Place Order
+              </Button>
+              <Button floated="right" color="blue" inverted onClick={resetCart}>
+                Clear Cart
+              </Button>
+            </>
+          )}
         </Container>
       </div>
     )
