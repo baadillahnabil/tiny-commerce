2a14ed7d054dc8f032f062ca2abacd63a399d57c
baadillahnabil <nabilzero27@gmail.com>
2019-03-15 15:32:16 +0700

    include navbar and cart page

--- src/Root/app.jsx
+++ src/Root/app.jsx
@@ -1,16 +1,22 @@
 import React, { Component, Suspense, lazy } from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
 
+// Components
+import Navbar from '../components/Navbar/navbar'
+
 // Pages
 const Home = lazy(() => import('../pages/Products/products'))
+const Carts = lazy(() => import('../pages/Carts/carts'))
 
 class App extends Component {
   render() {
     return (
       <>
         <Suspense fallback={<div>Loading...</div>}>
+          <Navbar />
           <Switch>
             <Route path="/" exact render={() => <Home />} />
+            <Route path="/carts" render={() => <Carts />} />
             <Route path="*" render={() => <Redirect to="/" />} />
           </Switch>
         </Suspense>
